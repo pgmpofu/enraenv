f6b85c3f550c36aa25a3caa0ef55d9ecac85002d
Patience Mpofu <pgmpofu@gmail.com>
2019-01-31 09:59:56 -0800

    Updated expired timer

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,7 @@
 //Shamelessly copied from www.w3schools.com and modified thereafter
 //What? Its what YOU would do.
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2018 00:00:00").getTime();
+var countDownDate = new Date("Jan 1, 2019 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
